8aced11dda8b2c531e90ba29b254e00455afe186
leolaz99 <leolaz99@gmail.com>
2020-12-02 16:27:22 +0100

    Finish Game

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+        tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        rb.AddForce(tilt, ForceMode.Acceleration);
+    }
+}
---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] GameObject winPanel;
+    [SerializeField] GameObject losePanel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            winPanel.SetActive(true);
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            losePanel.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
